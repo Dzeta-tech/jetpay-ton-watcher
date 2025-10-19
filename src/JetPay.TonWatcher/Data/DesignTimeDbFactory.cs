e53df4c168f9d98cbf3ad86ef1c2c831327454c5
Andrey Onishchenko <loraner123@gmail.com>
2025-10-19 23:44:44 +0300

    EF Migration

---
+++ src/JetPay.TonWatcher/Data/DesignTimeDbFactory.cs
@@ -0,0 +1,10 @@
+using JetPay.TonWatcher.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace JetPay.TonWatcher.Data;
+
+public class DesignTimeDbFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args) => new(new DbContextOptionsBuilder<ApplicationDbContext>().UseNpgsql("Server=localhost;Database=JetPay.TonWatcher;Username=postgres;Password=postgres").Options);
+}
