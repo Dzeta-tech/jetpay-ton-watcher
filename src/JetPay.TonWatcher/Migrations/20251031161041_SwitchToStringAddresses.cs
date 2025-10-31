bd46521197947c83edd20e24ed03fc2a3307145c
Andrey Onishchenko <loraner123@gmail.com>
2025-10-31 19:10:53 +0300

    Migration

---
+++ src/JetPay.TonWatcher/Migrations/20251031161041_SwitchToStringAddresses.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JetPay.TonWatcher.Migrations
+{
+    /// <inheritdoc />
+    public partial class SwitchToStringAddresses : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Hash",
+                table: "TrackedAddresses");
+
+            migrationBuilder.DropColumn(
+                name: "Workchain",
+                table: "TrackedAddresses");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "TrackedAddresses",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "TrackedAddresses");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Hash",
+                table: "TrackedAddresses",
+                type: "bytea",
+                maxLength: 32,
+                nullable: false,
+                defaultValue: new byte[0]);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Workchain",
+                table: "TrackedAddresses",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
