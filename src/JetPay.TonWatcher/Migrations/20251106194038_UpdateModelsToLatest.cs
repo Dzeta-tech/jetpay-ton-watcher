4badd5913fc17a5bc0b97a65103fbe0651c6fa80
Andrey Onishchenko <loraner123@gmail.com>
2025-11-06 22:40:52 +0300

    Create missing index

---
+++ src/JetPay.TonWatcher/Migrations/20251106194038_UpdateModelsToLatest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JetPay.TonWatcher.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateModelsToLatest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_ShardBlocks_IsProcessed_Seqno",
+                table: "ShardBlocks",
+                columns: new[] { "IsProcessed", "Seqno" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ShardBlocks_IsProcessed_Seqno",
+                table: "ShardBlocks");
+        }
+    }
+}
